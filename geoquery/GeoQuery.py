d1c9f282bf485ad3b118ce63b31544a337deef30
Philip Davis <philipdavis@Philips-MacBook-Pro-2.local>
2023-06-13 10:30:45 -0400

    Move MGRS IDX calc into library

--- geoquery/GeoQuery.py
+++ geoquery/GeoQuery.py
@@ -16,13 +16,71 @@ def char_range(c1, c2):
     for c in range(ord(c1), ord(c2)+1):
         yield chr(c)
 
-def get_tile(name):
+def GetTile(name):
     metadata = f.split('.')
     if metadata[-1] == 'tif' or metadata[-1] == 'tiff':
         tile = metadata[2][1:]
         return(tile[0:-2], tile[-2:])
     else:
         return(None, None)
+
+def CalcMGRSIdx():
+    tile_column = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    tile_row = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V']
+
+    m = mgrs.MGRS()
+    mgrs_idx = {}
+    for first in range(1, 61):
+        for second in char_range('A', 'Z'):
+            mgrs_idx[(first, second)] = {}
+            tiles = []
+            found_col = []
+            found_row = []
+            for third in tile_column:
+                for fourth in tile_row:
+                    mgrs_str = f'{first}{second}{third}{fourth}'
+                    try:
+                        latlon = m.toLatLon(mgrs_str)
+                        tiles.append((third, fourth))
+                        found_col.append(third)
+                        found_row.append(fourth)
+                    except Exception:
+                        pass 
+            low_col = None
+            low_row = None
+            if not tiles:
+                continue
+            if 'Z' not in found_col:
+                low_col = found_col[0]
+            else:
+                gap = False
+                for col in tile_column:
+                    if col not in found_col:
+                        gap = True
+                    else:
+                        if gap:
+                            low_col = col
+                            break
+            if 'Z' not in found_row:
+                low_row = found_row[0]
+            else:
+                gap = False
+                for row in tile_row:
+                    if row not in found_row:
+                        gap = True
+                    else:
+                        if gap:
+                            low_row = row
+                            break
+            for tile in tiles:
+                coffset = ord(tile[0]) - ord(low_col)
+                if coffset < 0:
+                    coffset = coffset + len(tile_column)
+                roffset = ord(tile[1]) - ord(low_row)
+                if roffset < 0:
+                    roffset = roffset + len(tile_row)
+                mgrs_idx[(first, second)][tile] = (coffset, roffset)
+    return(mgrs_idx) 
 
 def GetSRCoord(lon, lat, projection):
     source = osr.SpatialReference()
@@ -90,8 +148,7 @@ class EarthDataOSFAccess:
     def __init__(self, directory):
         self.project = directory
         self.osf = osfclient.OSF()
-        with open('mgrs_idx.pkl', 'rb') as mgrsf:
-            self.mdb = pickle.load(mgrsf)
+        self.mdb = CalcMGRSIdx()
         self.db = {}
         p = self.osf.project(self.project)
         for s in p.storages:
@@ -149,8 +206,7 @@ class EarthDataOSFAccess:
 class EarthDataLocalAccess:
     def __init__(self, directory):
         self.dir = directory
-        with open('mgrs_idx.pkl', 'rb') as mgrsf:
-            self.mdb = pickle.load(mgrsf)
+        self.mdb = CalcMGRSIdx()
         self.db = {}
         for first in os.listdir(self.dir):
             firstf = f'{directory}/{first}'
